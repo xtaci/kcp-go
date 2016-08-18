e33fd56ca179be0b3386198dff4d23e4780803c2
xtaci <daniel820313@gmail.com>
2016-08-18 22:55:42 +0800

    Revert "up"
    This reverts commit 577597c7ca8773c0bdb58b258d3eaca90487aeab.

--- kcp.go
+++ kcp.go
@@ -735,7 +735,6 @@ func (kcp *KCP) flush() {
 			} else {
 				segment.rto += kcp.rx_rto / 2
 			}
-			segment.rto = _imin_(segment.rto, 4*kcp.rx_rto)
 			segment.resendts = current + segment.rto
 			lost = true
 			lostSegs++
