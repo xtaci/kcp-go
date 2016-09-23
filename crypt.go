1ca4a767824a05274aedbe1231d37738242bd28e
xtaci <daniel820313@gmail.com>
2016-09-23 12:56:02 +0800

    add a comment to BlockCrypt

--- crypt.go
+++ crypt.go
@@ -20,7 +20,9 @@ var (
 	saltxor       = `sH3CIVoF#rWLtJo6`
 )
 
-// BlockCrypt defines encryption/decryption methods for a given byte slice
+// BlockCrypt defines encryption/decryption methods for a given byte slice.
+// Notes on implementing: the data to be encrypted contains a builtin
+// nonce at the first 16 bytes
 type BlockCrypt interface {
 	// Encrypt encrypts the whole block in src into dst.
 	// Dst and src may point at the same memory.
