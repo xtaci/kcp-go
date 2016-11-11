85ba2e42ba556bb896730be37139117c7d5f9bb9
xtaci <daniel820313@gmail.com>
2016-11-11 17:05:23 +0800

    Revert "fix a boundary"
    This reverts commit 6168f7381e3205fb5e17ee4752ff50848c14f7b8.

--- kcp.go
+++ kcp.go
@@ -635,7 +635,7 @@ func (kcp *KCP) flush() {
 			ptr = buffer
 		}
 		ack := heap.Pop(&kcp.acklist).(ackItem)
-		if ack.sn >= kcp.rcv_nxt || kcp.acklist.Len() == 0 {
+		if ack.sn > kcp.rcv_nxt || kcp.acklist.Len() == 0 {
 			seg.sn, seg.ts = ack.sn, ack.ts
 			ptr = seg.encode(ptr)
 		}
