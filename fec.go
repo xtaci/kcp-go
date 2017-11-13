025c14799b960c15f63c3fc41ca8f1915090d519
xtaci <daniel820313@gmail.com>
2017-11-14 02:12:57 +0800

    Revert "use xor.BytesSameLen"
    This reverts commit 3229d2ca59ff51f003ce6ee26a8ee382c484e51e.

--- fec.go
+++ fec.go
@@ -155,7 +155,7 @@ func (dec *fecDecoder) decode(pkt fecPacket) (recovered [][]byte) {
 				if shards[k] != nil {
 					dlen := len(shards[k])
 					shards[k] = shards[k][:maxlen]
-					xor.BytesSameLen(shards[k][dlen:], shards[k][dlen:], shards[k][dlen:])
+					xor.Bytes(shards[k][dlen:], shards[k][dlen:], shards[k][dlen:])
 				}
 			}
 			if err := dec.codec.ReconstructData(shards); err == nil {
@@ -265,7 +265,7 @@ func (enc *fecEncoder) encode(b []byte) (ps [][]byte) {
 		for i := 0; i < enc.dataShards; i++ {
 			shard := enc.shardCache[i]
 			slen := len(shard)
-			xor.BytesSameLen(shard[slen:enc.maxSize], shard[slen:enc.maxSize], shard[slen:enc.maxSize])
+			xor.Bytes(shard[slen:enc.maxSize], shard[slen:enc.maxSize], shard[slen:enc.maxSize])
 		}
 
 		// construct equal-sized slice with stripped header
