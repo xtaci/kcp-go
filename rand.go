0dc7be275bda3ba07791a211981b80f2af5d9361
xtaci <daniel820313@gmail.com>
2017-11-29 21:15:02 +0800

    remove the lock in rand

--- rand.go
+++ rand.go
@@ -4,7 +4,6 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"io"
-	"sync"
 )
 
 // nonceMD5 is a nonce generator for each packet header
@@ -12,18 +11,15 @@ import (
 // The benchmark shows it's faster than previous CSPRNG only method.
 type nonceMD5 struct {
 	data [md5.Size]byte
-	mu   sync.Mutex
 }
 
 // Nonce fills a nonce into the provided slice with no more than md5.Size bytes
-// the entropy will be updated whenenver a leading 0 is found
+// the entropy will be updated whenever a leading 0 appears
 func (n *nonceMD5) Fill(nonce []byte) {
-	n.mu.Lock()
 	if n.data[0] == 0 { // 1/256 chance for entropy update
 		io.ReadFull(rand.Reader, n.data[:])
 	}
 	n.data = md5.Sum(n.data[:])
 	copy(nonce, n.data[:])
-	n.mu.Unlock()
 	return
 }
