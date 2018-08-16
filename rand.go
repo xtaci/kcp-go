ebb64e277a97926a288dc3b74d23f384159f2b63
xtaci <daniel820313@gmail.com>
2018-08-16 14:40:05 +0800

    update comment

--- rand.go
+++ rand.go
@@ -13,7 +13,7 @@ type nonceMD5 struct {
 	data [md5.Size]byte
 }
 
-// Nonce fills a nonce into the provided slice with no more than md5.Size bytes
+// Fill fills a nonce into the provided slice with no more than md5.Size bytes
 // the entropy will be updated whenever a leading 0 appears
 func (n *nonceMD5) Fill(nonce []byte) {
 	if n.data[0] == 0 { // 1/256 chance for entropy update
