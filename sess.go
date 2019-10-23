b15c303be7ae7f7b759cd72b114602faee26af89
xjdrew <1454565+xjdrew@users.noreply.github.com>
2019-10-23 16:24:44 +0800

    try best to send all queued messages when close UDPSession (#145)
    * try best to send all queued messages when close UDPSession
    
    * fix: lock

--- sess.go
+++ sess.go
@@ -349,6 +349,12 @@ func (s *UDPSession) Close() error {
 	if once {
 		atomic.AddUint64(&DefaultSnmp.CurrEstab, ^uint64(0))
 
+		// try best to send all queued messages
+		s.mu.Lock()
+		s.kcp.flush(false)
+		s.uncork()
+		s.mu.Unlock()
+
 		if s.l != nil { // belongs to listener
 			s.l.closeSession(s.remote)
 			return nil
