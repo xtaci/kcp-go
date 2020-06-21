950fb4de46a94be122cbd74f726675fd34c187ff
lidecheng <lidecheng@qiniu.com>
2020-06-21 16:55:23 +0800

    add tcp-server test

---
+++ tcp-server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("checkError", err)
+		os.Exit(-1)
+	}
+}
+
+func handleEcho(conn *net.TCPConn) {
+	fmt.Println("handleEcho start", conn.RemoteAddr())
+	defer fmt.Println("handleEcho end", conn.RemoteAddr())
+
+	defer conn.Close()
+
+	buf := make([]byte, 4096)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		n, err = conn.Write(buf[:n])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func main() {
+	ch := make(chan int, 2)
+	ch <- 1
+	fmt.Println(len(ch), cap(ch))
+
+	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:7891")
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", addr)
+	checkError(err)
+
+	fmt.Println("server start")
+	for {
+		conn, err := listener.AcceptTCP()
+		checkError(err)
+		go handleEcho(conn)
+	}
+}
